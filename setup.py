adfc6a261a5afb3b0ba5aea1703e1b508c0851c1
Paulius Maruška <paulius.maruska@gmail.com>
2017-06-21 13:47:51 +0300

    Bumped version, added keywords and classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="python-isign",
-    version="0.1.2",
+    version="0.2.0",
     description="Python client for isign.io API.",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
@@ -13,5 +13,16 @@ setuptools.setup(
     install_requires=[
         "python-dateutil",
         "requests",
+    ],
+    keywords=["isign", "client"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries",
     ],
 )
